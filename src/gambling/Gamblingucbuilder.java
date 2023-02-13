4753b88e52a3c5f6aa738969450b945f11932dca
kundan571 <kundanchauhan1997@gmail.com>
2023-02-14 01:22:09 +0530

    Added: check either win or loose

--- src/gambling/Gamblingucbuilder.java
+++ src/gambling/Gamblingucbuilder.java
@@ -1,19 +1,21 @@
 package gambling;
 public class Gamblingucbuilder {
-    int STAKE_PER_DAY = 100;
-    int BET_PER_GAME = 1;
     public int winLoose() {
         return (int) (Math.floor(Math.random() * 2) + 1);
     }
     public static void main(String[] args) {
+        int STAKE_PER_DAY = 100;
+        int BET_PER_GAME = 1;
         Gamblingucbuilder gamble = new Gamblingucbuilder();
         int winLoose = gamble.winLoose();
         int gambler_Amount = 0;
         if (winLoose == 1) {
             System.out.println(winLoose + " You win the bet");
+            System.out.println(STAKE_PER_DAY+1  );
             gambler_Amount++;
         } else {
             System.out.println(winLoose + " You loose the bet");
+            System.out.println(STAKE_PER_DAY-1);
             gambler_Amount--;
         }
 
