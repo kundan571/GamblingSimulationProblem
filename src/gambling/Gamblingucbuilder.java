bd35322c74395505c51547d8e4a4530134ee7574
kundan571 <kundanchauhan1997@gmail.com>
2023-02-14 13:06:42 +0530

    Added: win or loose is the fifty percent

--- src/gambling/Gamblingucbuilder.java
+++ src/gambling/Gamblingucbuilder.java
@@ -1,23 +1,30 @@
 package gambling;
 public class Gamblingucbuilder {
-    public int winLoose() {
-        return (int) (Math.floor(Math.random() * 2) + 1);
-    }
+//    public int winLoose() {
+//        return (int) (Math.floor(Math.random() * 2) + 1);
+//    }
+
     public static void main(String[] args) {
         int STAKE_PER_DAY = 100;
         int BET_PER_GAME = 1;
-        Gamblingucbuilder gamble = new Gamblingucbuilder();
-        int winLoose = gamble.winLoose();
-        int gambler_Amount = 0;
-        if (winLoose == 1) {
-            System.out.println(winLoose + " You win the bet");
-            System.out.println(STAKE_PER_DAY+1  );
-            gambler_Amount++;
-        } else {
-            System.out.println(winLoose + " You loose the bet");
-            System.out.println(STAKE_PER_DAY-1);
-            gambler_Amount--;
-        }
+      //  Gamblingucbuilder gamble = new Gamblingucbuilder();
+       // int winLoose = gamble.winLoose();
+        int winamount = 0;
+        int winLoose = 0;
+        for (winamount = 0; winamount <= 50; winamount++){
+            winLoose = (int) (Math.floor(Math.random() * 2) + 1);
 
+            if (winLoose == 1) {
+                System.out.println(winLoose + " You win the bet");
+                winLoose++;
+
+            } else {
+                System.out.println(winLoose + " You loose the bet");
+                winLoose--;
+
+            }
+
+        }
     }
 }
+
